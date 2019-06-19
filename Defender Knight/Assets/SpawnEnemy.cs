f31ae655da27cfe6410f2e6ed832b85ca4ccbb17
zeekkeen <zekken2307@gmail.com>
2019-06-18 21:52:53 -0400

    spawn con enemigos aleatorios y la cantidad de enemigos generados mas el tiempo de espera para el spawn

--- Defender Knight/Assets/SpawnEnemy.cs
+++ Defender Knight/Assets/SpawnEnemy.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class SpawnEnemy : MonoBehaviour
 {
-    public GameObject enemy;
+    public GameObject[] enemy;
     bool Spawning = false;
     public float SpawnSpeed;
+    public int cantidad;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +17,11 @@ public class SpawnEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!Spawning) StartCoroutine(Spawn(SpawnSpeed));
+        if (cantidad>0)
+        {
+            if (!Spawning) StartCoroutine(Spawn(SpawnSpeed));
+
+        }
     }
 
     IEnumerator Spawn(float seconds)
@@ -25,10 +30,11 @@ public class SpawnEnemy : MonoBehaviour
         // Si el prefab es correcto creamos el enemigo
         if (enemy != null)
         {
-            GameObject aux = Instantiate(enemy, transform.position, transform.rotation);
+            GameObject aux = Instantiate(enemy[(int)Random.Range(0,3)], transform.position, transform.rotation);
             // Esperamos los segundos de turno antes de hacer spawn de otro enemigo
             yield return new WaitForSeconds(seconds);
         }
         Spawning = false; // Desactivamos la bandera
+        cantidad--;
     }
 }
